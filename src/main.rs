d1a4cab644f7cbbe7e3d939cf4217826865ff9d7
Sebastian Tremmel <littleperlmonk@gmail.com>
2021-11-20 17:39:44 +0100

    feat: adding command line parameters

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use type_cli::CLI;
+
 fn main() {
-    println!("Hello, world!");
+    let RustWeather { zip, country } = RustWeather::process();
+    eprint!("zip:{}, country:{} ", zip, country)
+}
+
+#[derive(CLI)]
+struct RustWeather {
+    #[named(short = "z")]
+    zip: String,
+
+    #[named(short = "c")]
+    country: String,
 }
